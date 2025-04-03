9fc8de60e70db9b78abb01571c208baff73fd6e1
Ojaswee Dhungana <ojaswee@outlook.com>
2025-04-03 11:53:54 -0700

    added handle user events

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,67 @@
+import { useState } from "react";
+
 export default function UserInput() {
+	const [UserInput, setUserInput] = useState({
+		investmentAmount: 10000,
+		annualInverstment: 1200,
+		expectedReturn: 6,
+		years: 10,
+	});
+
+	function handleChange(inputIdentifier, newValue) {
+		setUserInput((prevUserInput) => {
+			return { ...prevUserInput, [inputIdentifier]: newValue };
+		});
+	}
+
 	return (
 		<section id="user-input">
 			<div className="input-group">
-				<div>
+				<p>
 					<label htmlFor="investment-amount">Investment Amount:</label>
-					<input type="number" id="investment-amount" name="investment-amount" required />
-				</div>
-				<div>
-					<label htmlFor="interest-rate">Interest Rate (%):</label>
-					<input type="number" id="interest-rate" name="interest-rate" required />
-				</div>
-				<div>
+					<input
+						type="number"
+						id="investment-amount"
+						name="investment-amount"
+						value={UserInput.investmentAmount}
+						onChange={(event) => handleChange("investmentAmount", event.target.value)}
+					/>
+				</p>
+				<p>
+					<label htmlFor="annual-investent">Annual Investment:</label>
+					<input
+						type="number"
+						id="annual-investent"
+						name="annual-investent"
+						value={UserInput.annualInverstment}
+						onChange={(event) => handleChange("annualInverstment", event.target.value)}
+						required
+					/>
+				</p>
+			</div>
+			<div className="input-group">
+				<p>
 					<label htmlFor="expected-return">Expected Return:</label>
-					<input type="number" id="expecteed-return" name="expecteed-return" required />
-				</div>
-				<div>
+					<input
+						type="number"
+						id="expected-return"
+						name="expected-return"
+						value={UserInput.expectedReturn}
+						onChange={(event) => handleChange("expectedReturn", event.target.value)}
+						required
+					/>
+				</p>
+				<p>
 					<label htmlFor="years">Years:</label>
-					<input type="number" id="years" name="years" required />
-				</div>
-				<button type="submit">Calculate</button>
+					<input
+						type="number"
+						id="years"
+						name="years"
+						value={UserInput.years}
+						onChange={(event) => handleChange("years", event.target.value)}
+						required
+					/>
+				</p>
 			</div>
 		</section>
 	);
