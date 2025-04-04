eb4f47ca40b2352c40a9f69b00293f08e3004b2e
Ojaswee Dhungana <ojaswee@outlook.com>
2025-04-04 14:34:25 -0700

    error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,40 @@ import Results from "./components/Results.jsx";
 
 function App() {
 
-  const [userInput, setUserInput] = useState({
+	const [userInput, setUserInput] = useState({
 		investmentAmount: 10000,
 		annualInvestment: 1200,
 		expectedReturn: 6,
 		years: 10,
 	});
 
-  function handleChange(inputIdentifier, newValue) {
+	const inputIsValid = (input) => {
+		return (
+			input.investmentAmount > 0 &&
+			input.annualInvestment > 0 &&
+			input.expectedReturn > 0 &&
+			input.years > 0
+		);
+	};
+
+	function handleChange(inputIdentifier, newValue) {
 		setUserInput((prevUserInput) => {
 			return { ...prevUserInput, [inputIdentifier]: +newValue };
 		});
 	}
 
-  return (
-    <div>
-      <TheHeader />
-      <UserInput UserInput={userInput} onChange={handleChange}/>
-      <Results input={userInput}/>
-    </div>
-  )
+	return (
+		<div>
+			<TheHeader />
+			<UserInput UserInput={userInput} onChange={handleChange} />
+			{!inputIsValid(userInput) && (
+				<p className="center">
+					Please enter valid numbers (greater than 0).
+				</p>
+			)}
+			{inputIsValid(userInput) && <Results input={userInput} />}
+		</div>
+	)
 }
 
 export default App;
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,36 @@
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
 	const results = calculateInvestmentResults(input);
+	const initalInverstment = results[0].valueEndOfYear - results[0].interest - results[0].annualInvestment;
+	console.log(results);
 	return (
-		console.log(results),
-		<p>Results</p>
-	)
+		<table id="result">
+			<thead>
+				<tr>
+					<th>Year</th>
+					<th>Investment Value</th>
+					<th>Interest (Year)</th>
+					<th>Total Interest</th>
+					<th>Intevested Capital</th>
+				</tr>
+			</thead>
+            <tbody>
+                {results.map((yearData) => {
+                    const totalInterest =
+                        yearData.valueEndOfYear - yearData.annualInvestment * yearData.year -initalInverstment;
+					const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+                    return (
+                        <tr key={yearData.year}>
+                            <td>{yearData.year}</td>
+                            <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                            <td>{formatter.format(yearData.interest)}</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+		</table>
+	);
 }
